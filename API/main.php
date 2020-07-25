79d4e29164a316c0fb935a2f62e3dc39a437c48b
Gostrate <gostrate5@gmail.com>
2020-07-25 15:57:33 +0800

    basic api

---
+++ API/main.php
@@ -0,0 +1,30 @@
+<?php 
+switch($_GET("view")){
+    case "annoucement":
+    $json=[
+        {
+          "announcementType": "活動",
+          "announcemntTitle": "公告標題",
+          "announcementText": "公告內文",
+          "announcementTime": "公告時間"
+        },
+        {
+          "announcementType": "優惠",
+          "announcemntTitle": "公告標題",
+          "announcementText": "公告內文",
+          "announcementTime": "公告時間"
+        },
+        {
+          "announcementType": "公告",
+          "announcemntTitle": "公告標題",
+          "announcementText": "公告內文",
+          "announcementTime": "公告時間"
+        }
+      ]
+        echo $json;
+    break;
+}
+
+
+
+?>
